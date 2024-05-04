bc1c5f66599c6eaf0fa91e6febea77092f353f77
Naveen Kumar <nvnkr09@gmail.com>
2024-05-04 13:17:36 +0530

    submit-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+// import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const apiKey = '99eb9fd1';
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${input}`);
+      const data = await response.json();
+      console.log(response);
+      console.log(data);
+      setMovies(data.Search);
+    } catch (error) {
+      console.error('Error fetching movies:', error.message);
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Search Movie</p>
+        <input type="text" onChange={(e)=> setInput(e.target.value)} />
+        <button onClick={fetchMovies}>Search</button>
+        <ul>
+        {movies.map((movie) => (
+            <li key={movie.imdbID}>
+              <p>{movie.Title} ({movie.Year})</p>
+              <img src={movie.Poster} alt="movie-image" />
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
 
-export default App
+export default App;
